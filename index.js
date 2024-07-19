666a27fa77bddba57ffc65dd42e2846bf603ea68
IgnorantOracle <deidra.clay15@gmail.com>
2024-07-18 20:40:56 -0400

    first push - brains done, still designing

--- index.js
+++ index.js
@@ -1,2 +1,62 @@
-const container = (document.querySelector('.container').innerText =
-  'mmmmmmmmm doughnuts.... ahhhhhhh!');
+const mobileMainBar = document.getElementById("mobile-main-bar");
+const sideBar = document.getElementById("sidebar");
+let numberOfDonuts = 0;
+let numberOfAutoClickers = 0;
+let autoClickerCost = 100;
+
+document.getElementById("menu-icon").onclick = function () {
+    sideBar.style.display = 'flex';
+}
+
+document.getElementById("sidebar-close").onclick = function () {
+    sideBar.style.display = 'none';
+    mobileMainBar.style.display = 'flex';
+}
+
+document.getElementById("cookie-clicker-main-link").onclick = function () {
+    const cookieClickerSection = document.getElementById("cookie-clicker");
+    cookieClickerSection.style.display = 'flex';
+}
+
+document.getElementById("donut-maker-button").onclick = function () {
+    numberOfDonuts = parseInt(numberOfDonuts) + 1;
+    document.getElementById("number-of-donuts").innerText = numberOfDonuts;
+}
+
+document.getElementById("reset").onclick = function () {
+    numberOfDonuts = 0;
+    numberOfAutoClickers = 0;
+    document.getElementById("number-of-donuts").innerText = numberOfDonuts;
+    document.getElementById("number-of-auto-clickers").innerText = numberOfAutoClickers;
+}
+
+document.getElementById("buy-auto-clicker").onclick = function () {
+    numberOfDonuts -= parseInt(autoClickerCost);
+    numberOfAutoClickers += 1;
+
+    document.getElementById("number-of-auto-clickers").innerText = numberOfAutoClickers;
+    document.getElementById("number-of-donuts").innerText = numberOfDonuts;
+
+    if (numberOfDonuts < autoClickerCost) {
+        document.getElementById("buy-auto-clicker").style.display = 'none';
+        document.getElementById("disabled-buy-auto-clicker").style.display = 'flex';
+    }
+
+    let autoClickerCostIncrease = autoClickerCost * 0.1;
+    autoClickerCost += autoClickerCostIncrease;
+}
+
+setInterval(function () {
+    if (numberOfAutoClickers > 0) {
+        numberOfDonuts += numberOfAutoClickers * 1;
+        document.getElementById("number-of-donuts").innerText = parseInt(numberOfDonuts);
+    }
+    
+}, 1000)
+
+setInterval(function () {
+    if (numberOfDonuts >= autoClickerCost) {
+        document.getElementById("disabled-buy-auto-clicker").style.display = 'none';
+        document.getElementById("buy-auto-clicker").style.display = 'flex';
+    }
+}, 1)
